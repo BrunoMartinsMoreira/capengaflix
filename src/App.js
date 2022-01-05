424aaffb59e7bf569bd22ac6f5793b0516927b12
Bruno Moreira <bruno_moreira.19@outlook.com>
2022-01-04 22:28:25 -0300

    iniciando css e adicionando os primeiros estilos

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import "./App.css";
 import MovieSection from "./components/MovieSection/MovieSection";
 import TmDbConfig from "./TmDbConfig";
 
--- src/components/MovieSection/MovieSection.js
+++ src/components/MovieSection/MovieSection.js
@@ -4,17 +4,20 @@ import styles from "./MovieSection.module.css";
 const MovieSection = ({ title, items }) => {
   const imgUrl = `https://image.tmdb.org/t/p/w300`;
   return (
-    <div>
+    <div className={styles.movieSection}>
       <h2>{title}</h2>
-      <div>
-        {items.results.length > 0 &&
-          items.results.map((item, key) => (
-            <img
-              key={key}
-              src={`${imgUrl}${item.poster_path}`}
-              alt={item.original_title}
-            />
-          ))}
+      <div className={styles.movieArea}>
+        <div className={styles.movieList}>
+          {items.results.length > 0 &&
+            items.results.map((item, key) => (
+              <div className={styles.movieItem} key={key}>
+                <img
+                  src={`${imgUrl}${item.poster_path}`}
+                  alt={item.original_title}
+                />
+              </div>
+            ))}
+        </div>
       </div>
     </div>
   );

23ff0c792c65b8df833f6faa26241f00102201db
Bruno Moreira <bruno_moreira.19@outlook.com>
2022-01-04 21:21:10 -0300

    criando estado de listagem de filmes e estrutura do componente de filmes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import MovieItem from "./components/MovieItem/MovieItem";
 import TmDbConfig from "./TmDbConfig";
 
 const App = () => {
+  const [movieList, setMovieList] = useState([]);
+
   useEffect(() => {
     const loadAll = async () => {
       let list = await TmDbConfig.getHomeList();
-      console.log(list);
+      setMovieList(list);
     };
     loadAll();
   }, []);
 
-  return <div className="App">Olaaaa</div>;
+  return (
+    <div className="App">
+      <section>
+        {movieList.map((item, key) => (
+          <MovieItem key={key} />
+        ))}
+      </section>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/MovieItem/MovieItem.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./MovieItem.module.css";
+
+const MovieItem = () => {
+  return <div>aoba</div>;
+};
+
+export default MovieItem;
